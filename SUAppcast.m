b7fff227695adf19c5abe3a088858adabd484bcc
James Beith <james@beith.com>
2014-03-31 11:14:01 +0100

    Test value before attempting to insert it in to the dictionary (note: following the current coding style for this class).

--- SUAppcast.m
+++ SUAppcast.m
@@ -194,7 +194,9 @@
 				else if (name != nil)
 				{
 					// add all other values as strings
-					[dict setObject:[[node stringValue] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] forKey:name];
+					NSString *nodeValue = [[node stringValue] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+					if (nodeValue)
+						[dict setObject:nodeValue forKey:name];
 				}
             }
             
